7c262fd3cadadbbaac546c5accbfb5c8eca03bdc
BuildTools <seantgrover@gmail.com>
2019-08-09 16:55:59 -0600

    Add Cross Origin

--- src/main/java/com/chestermere/lake/temperature/controllers/LatestController.java
+++ src/main/java/com/chestermere/lake/temperature/controllers/LatestController.java
@@ -1,5 +1,6 @@
 package com.chestermere.lake.temperature.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.chestermere.lake.temperature.Application;
 import com.chestermere.lake.temperature.objects.Snapshot;
 
 @RestController
+@CrossOrigin
 public class LatestController {
 
 	/**
