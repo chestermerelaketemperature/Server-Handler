077256fa6ae690be60f846fa5170460f58245a79
LimeGlass <seantgrover@gmail.com>
2020-06-10 00:15:01 -0600

    Switch protocol from Java sockets to Japson

--- src/main/java/com/chestermere/lake/temperature/Server.java
+++ src/main/java/com/chestermere/lake/temperature/Server.java
@@ -2,6 +2,9 @@ package com.chestermere.lake.temperature;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import javax.annotation.Nullable;
 
@@ -9,20 +12,20 @@ import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 
-import com.chestermere.lake.temperature.managers.HandlerManager;
 import com.chestermere.lake.temperature.managers.SnapshotManager;
-import com.chestermere.lake.temperature.sockets.SocketListener;
 import com.chestermere.lake.temperature.utils.Encryption;
 import com.google.common.flogger.FluentLogger;
+import com.sitrica.japson.gson.JsonObject;
+import com.sitrica.japson.server.JapsonServer;
+import com.sitrica.japson.shared.Handler;
 
 public class Server {
 
 	private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-	private final SocketListener socketListener;
-	private final HandlerManager handlerManager;
 	private final SnapshotManager snapshots;
 	private final Encryption encryption;
 	private Configuration configuration;
+	private JapsonServer japson;
 
 	Server() {
 		Configurations configurations = new Configurations();
@@ -33,17 +36,24 @@ public class Server {
 			exception.printStackTrace();
 		}
 		this.encryption = new Encryption(this);
-		this.socketListener = new SocketListener(this);
-		this.handlerManager = new HandlerManager(this);
 		this.snapshots = new SnapshotManager(this);
-	}
-
-	public HandlerManager getHandlerManager() {
-		return handlerManager;
-	}
-
-	public SocketListener getSocketListener() {
-		return socketListener;
+		try {
+			japson = new JapsonServer(1337);
+			japson.registerHandlers(new Handler(0x01) {
+				@Override
+				public String handle(InetAddress address, int port, JsonObject object) {
+					if (!object.has("temperature"))
+						return null;
+					float temperature = object.get("temperature").getAsFloat();
+					//TODO handle air temperature into SnapshotManager.
+					// Use a public REST API for now.
+					snapshots.addSnapshot(false, temperature, 0);
+					return null;
+				}
+			});
+		} catch (UnknownHostException | SocketException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Nullable
